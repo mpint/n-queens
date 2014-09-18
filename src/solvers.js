6b5956db9dc60c27b33a096d2d825e606a28837c
mpint <mpinter09@gmail.com>
2014-09-17 20:36:47 -0700

    put some pseudocode into algorithm

--- src/solvers.js
+++ src/solvers.js
@@ -15,7 +15,19 @@
 
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
+  var board = new Board({'n': n});
+  var rows = board.rows();
 
+  // got ^^ dem rows up dere ^^^
+  // place rook in top left most avaiable position. It iterates by going right along the row, then down.
+  // it puts itself down in the first conforming spot, puts itself down at the n farther step.
+  // it puts the lotion on its skin
+  // it truth tests - WHO AM I?
+  //COMMITS SUICIDE
+  //THERE ARE NO ROOKS, THEY'RE ALL DEAD
+  // tests pass
+
+  // place second rook in closes
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
